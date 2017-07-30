5eaf3100fe96246e4ede456c7bd0ebc79a2dace6
ajeeb <to.ajeeb@gmail.com>
2017-07-30 11:24:17 +0530

    modified

--- pyCode/fun2.py
+++ pyCode/fun2.py
@@ -1,5 +1,7 @@
-def print_msg(msg,error = "No error"):
-	print ("Log: "+error+"\t"+msg)
+def print_msg(msg,error = "No error",*kwargs):
+	print ("Log: "+error+":"+msg)
+	print(kwargs)
 
-print_msg("122323")
-print_msg("123456", "File not found")
+#print_msg("122323")
+#print_msg("123456", "File not found")
+print_msg("123456","File not found","1","2","3434")
