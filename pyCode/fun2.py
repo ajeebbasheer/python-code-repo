687fbf9cc62a0cd8ab24cca0776333e7ab21c071
ajeeb <to.ajeeb@gmail.com>
2017-07-30 11:29:37 +0530

    modified

--- pyCode/fun2.py
+++ pyCode/fun2.py
@@ -5,3 +5,7 @@ def print_msg(msg,error = "No error",*kwargs):
 #print_msg("122323")
 #print_msg("123456", "File not found")
 print_msg("123456","File not found","1","2","3434")
+
+#single * = tuple
+#double ** = dictionary
+#print_msg("12334","File not found","key=1","hey2=2")
