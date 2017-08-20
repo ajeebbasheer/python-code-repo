576057f184dc57782627a53ef21ef5feffd3965a
ajeeb <to.ajeeb@gmail.com>
2017-08-20 10:39:15 +0530

    added

--- pyCode/argp.py
+++ pyCode/argp.py
@@ -1,5 +1,20 @@
-import argparse 
-parser= argparse.ArgumentParser()
-parser.add_argument("hi")
-args = parser.parse_args()
-print(args.hi)
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-a', nargs=2)
+ap.add_argument('-b', nargs=3)
+ap.add_argument('-c', nargs=1)
+
+opts = ap.parse_args('-a A1 A2 -b B1 B2 B3 -c C1'.split())
+
+print(opts)
+print(opts.a)
+print(opts.b)
+print(opts.c)
+
+opts = ap.parse_args([])
+if not any([opts.a, opts.b, opts.c]):
+    ap.print_usage()
+    quit()
+
+print("This won't run.")
